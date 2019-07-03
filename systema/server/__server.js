de1a35939ddab8e764ccbc3a93d7f605244d4e21
root <root@stat-server.us-central1-a.c.bhairav-202703.internal>
2019-07-03 17:15:40 +0000

    Added thrift rpc interface in SystemA as well

--- systema/server/__server.js
+++ systema/server/__server.js
@@ -1,4 +1,5 @@
-const express=require('express');
+const express = require('express');
+const request = require('request');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
@@ -20,17 +21,38 @@ app.use(logger('dev'));
 // this method fetches all available data in our database
 router.get('/isValidEntry', (req, res) => {
   
-  return res.json({ success: true, data: req });
+  return res.json({ success: true});
 
 
 });
 
-router.get('/testCalls', function (req, res) { return res.json({1:0}) });
+router.get('/genRand', (req,res) => {
+
+	request('http://34.68.230.109:9090/generateNums', function(error,response,body) {
+	
+		res.json({nums:body});
+	
+	});
+});
+router.get('/calculateStats',(req,res) => {
+	
+	request('http://34.68.230.109:9090/calculateStats?'+req.query.allNums, function(error,response,body){
+		var result = body.split(",");
+		var mean,variance,stddev=0.0;
+		var median=0;
+		mean = parseFloat(result[0].split(":")[1]);
+		median = parseInt(result[1].split(":")[1]);
+		variance = parseFloat(result[2].split(":")[1]);
+		stddev = parseFloat(result[3].split(":")[1]);
+		res.json({mean:mean,median:median,variance:variance,stddev:stddev});
+	});
+
+});
+
+
 
 // append /api for our http requests
 app.use('/api', router);
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
-
-
--- systema/server/server.js
+++ systema/server/server.js
@@ -3,7 +3,6 @@ const request = require('request');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-//const Data = require('./data');
 
 const API_PORT = 3001;
 const app = express();
@@ -11,44 +10,80 @@ app.use(cors());
 const router = express.Router();
 
 
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
 
-// this is our get method
-// this method fetches all available data in our database
-router.get('/isValidEntry', (req, res) => {
-  
-  return res.json({ success: true});
 
+var thrift = require('thrift');
+var statserver = require('./gen-nodejs/StatServer');
+var ttypes = require('./gen-nodejs/statserver_types');
+const assert = require('assert');
+
+var transport = thrift.TBufferedTransport;
+var protocol = thrift.TBinaryProtocol;
+
+var connection = thrift.createConnection("10.128.0.3", 9090, {
+  transport : transport,
+  protocol : protocol
+});
+
+connection.on('error', function(err) {
+  assert(false, err);
+});
+
+// Create a Calculator client with the connection
+var client = thrift.createClient(statserver, connection);
+
+
+router.get('/checkSanity', (req,res) => {
+       
+	
+	//res.json({error:JSON.stringify(req.body)});
+	var result = 'all okay!'
+ 	var numList= req.query.allNums.split(',');
+	if(numList.length!=10)
+		res.json({error:'Please enter 10 numbers'});
+	for(var i=0;i<numList.length;i++)
+		if(isNaN(numList[i]))
+			result = 'Please enter only numbers';
+        res.json({error:result});
+ 
+});
+router.get( '/calculateStats' , (req, res) => {
+	var stats="";
+	var allNumbers= req.query.allNums.split(',');
+	client.calculateStat(allNumbers, function(err, response) {
+  console.log('ping() resp : '+response);
+		if (err) {
+    console.log("InvalidOperation " + err);
+  } else {
+    console.log('Whoa? You know how to divide by zero?'+JSON.stringify(response));
+	  stats = JSON.stringify(response);
+	  res.setHeader('Content-Type', 'application/json');
+  	  res.json(response);
+  }
+  });
+	//res.json(stats);
+
+});
+router.get('/pingTest', (req,res) => {
+
+        client.ping(function(err, response) {
+  console.log('ping() resp : '+response);
+               res.json({ping:response});
+  });
 
 });
 
 router.get('/genRand', (req,res) => {
 
-	request('http://34.68.230.109:9090/generateNums', function(error,response,body) {
-	
-		res.json({nums:body});
-	
-	});
-});
-router.get('/calculateStats',(req,res) => {
-	
-	request('http://34.68.230.109:9090/calculateStats?'+req.query.allNums, function(error,response,body){
-		var result = body.split(",");
-		var mean,variance,stddev=0.0;
-		var median=0;
-		mean = parseFloat(result[0].split(":")[1]);
-		median = parseInt(result[1].split(":")[1]);
-		variance = parseFloat(result[2].split(":")[1]);
-		stddev = parseFloat(result[3].split(":")[1]);
-		res.json({mean:mean,median:median,variance:variance,stddev:stddev});
-	});
+        client.generateNums(function(err, response) {
+        res.json({nums:response});
+
+        });
 
 });
-
 
 
 // append /api for our http requests
